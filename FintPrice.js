287eb054ae8c68a6d8d48729d471f62298855b0f
tshepomelesi <twmelesi@gmail.com>
2024-12-12 19:31:47 +0200

    initialcommit

---
+++ FintPrice.js
@@ -0,0 +1,91 @@
+class FintPrice{
+        static VAT = 1.15
+        static product = null
+
+        static setProduct(){
+                this.product = {
+                        name : "product A",
+                        description : "product holder",
+                        price : 0,
+                        margin : 0.5,
+                        expenses : [
+                                {name : "Expense", comment : "expense holder", amount : 0},
+                                {name : "Expense", comment : "expense holder", amount : 0}
+                        ],
+                        VATprice : 0
+                }
+        }
+
+        // methods for name property
+        static setName(name){
+                // check if name is empty
+                if(!name) return
+
+                this.product.name = name
+                
+                return this.product
+        }
+        static getName(){
+                return this.product.name
+        }
+
+        // methods for price property
+        static setPrice(price){
+                if(!price) return
+
+                this.product.price = price
+
+                return this.product
+        }
+        static getPrice(){
+                this.calculatePrice()
+
+                return this.product.price
+        }
+        static calculatePrice(){
+                let expenses = 0
+                let price = 0
+
+                // accumulate expense costs
+                for(let i = 0; i < this.getExpenses().length; i++){
+                        expenses += this.getExpenses()[i].amount
+                }
+
+                // calculate the price
+                price = expenses * (1 + this.getMargin())
+
+                this.setPrice(price)
+
+                return this.product
+        }
+
+        // methods for margin property
+        static setMargin(margin){
+                if(!margin) return 
+
+                this.product.margin = margin
+
+                return this.product
+        }
+        static getMargin(){
+                return this.product.margin
+        }
+        // methods for expenses property
+        static setExpense(
+                {
+                        name = "Expense", 
+                        comment = "expense holder", 
+                        amount = 0
+                } = {}
+        ){
+                this.product.expenses.push({name, comment, amount})
+
+                return this.product
+        }
+        static getExpenses(){
+                return this.product.expenses
+        }
+        static print(){
+                console.log(this.product)
+        }
+}
---
+++ main.js
@@ -0,0 +1,22 @@
+// initialize or setup a dummy product first
+FintPrice.setProduct()
+
+// const product = FintPrice.setName("lemon")
+
+// // set expense
+// FintPrice.setExpense(
+//         {
+//                 name : "Material - Wood", 
+//                 comment : "White Oak 4 x 6",
+//                 amount : 640
+//         })
+
+// // set margin
+// FintPrice.setMargin(1)
+// console.log(FintPrice.getPrice())
+
+const handleCalculate = () => {
+        // get data from inputs
+        // pass data to the FintPrice class
+        // display return product data
+}
